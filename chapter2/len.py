7d0e71b33f9a493fe764fd6eeb38778d561f24ac
brk9009 <warsend23@hotmail.com>
2019-12-27 22:16:37 -0600

    length

---
+++ chapter2/len.py
@@ -0,0 +1,9 @@
+dog1 = 'Jaxx'
+dog2 = 'Cutie Pie'
+print(dog1, 'has', len(dog1), 'letters.')
+print(dog2, 'has', len(dog2), 'letters.')
+
+a_str = ''.join(reversed('Wow,Bob,wow!'))
+print(a_str)
+b_str = ''.join(sorted('Wow,Bob,wow!'))
+print(b_str)
